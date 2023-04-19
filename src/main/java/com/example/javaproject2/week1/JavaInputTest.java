eb01b9018dc5c6999191f5ef62c18ffc896fb195
shinjaewon99 <tlswodnjs99@naver.com>
2023-04-19 14:20:09 +0900

    feat : JavaInput, JavaInputTest 작업 - InputStream을 사용하여 입력, 출력

---
+++ src/main/java/com/example/javaproject2/week1/JavaInputTest.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.week1;
+
+import java.io.IOException;
+
+public class JavaInputTest {
+    public static void main(String[] args) throws IOException {
+        JavaInput input = new JavaInput();
+        input.readTwoChars();
+    }
+}
