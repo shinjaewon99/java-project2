f8a9a7f2c162d4cdf6ad4206724cda34deebb5e2
shinjaewon99 <tlswodnjs99@naver.com>
2023-04-19 15:28:40 +0900

    feat : ScannerEx, ScannerExTest 작업 - Scanner을 사용하여 입력, 출력

---
+++ src/main/java/com/example/javaproject2/week1/ScannerExTest.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.week1;
+
+public class ScannerExTest {
+
+    public static void main(String[] args) {
+        ScannerEx sc = new ScannerEx();
+
+        sc.readTwoNumbersAndPlus();
+    }
+}
