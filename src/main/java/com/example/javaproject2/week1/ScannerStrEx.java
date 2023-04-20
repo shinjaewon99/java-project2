e19f182d66c3d7606313f523ea47febf32d0c677
shinjaewon99 <tlswodnjs99@naver.com>
2023-04-20 11:20:41 +0900

    feat : 출력에 대한 설명을 추가 - 두개의 숫자를 입력하세요 :

--- src/main/java/com/example/javaproject2/week1/ScannerStrEx.java
+++ src/main/java/com/example/javaproject2/week1/ScannerStrEx.java
@@ -6,6 +6,13 @@ public class ScannerStrEx {
 
     void inputStringAndPlus() {
         Scanner in = new Scanner(System.in);
-        System.out.println(in.next() + in.next());
+
+        System.out.println("두개의 숫자를 입력하세요, 입력하고 엔터 입력하고 엔터를 해주세요");
+        System.out.print("첫번째 숫자 : ");
+        System.out.println("첫번째 숫자는" + in.nextInt());
+
+        System.out.print("두번째 숫자 : ");
+        System.out.println("두번째 숫자는" + in.nextInt());
+
     }
 }
