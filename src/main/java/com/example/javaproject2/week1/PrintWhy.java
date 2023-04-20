fa563e6f9fbb1fb28192b3d8c6f56a0ffacc72cb
shinjaewon99 <tlswodnjs99@naver.com>
2023-04-20 11:19:19 +0900

    feat : 클래스를 나누지 않고 한 클래스에서 main까지 호출 - 한 클래스에서 구현

---
+++ src/main/java/com/example/javaproject2/week1/PrintWhy.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.week1;
+
+public class PrintWhy {
+
+    static void print() {
+        System.out.println("Why");
+    }
+
+    public static void main(String[] args) {
+        print();
+    }
+}
