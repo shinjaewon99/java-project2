07432bcfa7d88cabbb4f4fa28db8008f46e4d1f0
shinjaewon99 <tlswodnjs99@naver.com>
2023-04-20 09:24:45 +0900

    feat : ScannerPrac 구현 - 두개의 숫자를 입력받아 더하기 구현

---
+++ src/main/java/com/example/javaproject2/week1/ScannerPrac.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.week1;
+import java.util.Scanner;
+public class ScannerPrac {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println(in.nextInt() + in.nextInt());
+
+    }
+}
