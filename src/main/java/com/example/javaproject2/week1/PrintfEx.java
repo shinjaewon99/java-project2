1b1f9f6e63ca1af299c4d7a75769d70421678ad4
shinjaewon99 <tlswodnjs99@naver.com>
2023-04-20 11:19:53 +0900

    feat : Printf를 사용하여 출력 - printf를 사용하여 출력

---
+++ src/main/java/com/example/javaproject2/week1/PrintfEx.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.week1;
+
+public class PrintfEx {
+
+    public void printf() {
+        System.out.printf("%s", "Hello");
+    }
+
+    public static void main(String[] args) {
+        PrintfEx printfEx = new PrintfEx(); // PrintEx 나와라
+        printfEx.printf(); // printEx야 물대포(메서드)를 쏴줘
+    }
+}
