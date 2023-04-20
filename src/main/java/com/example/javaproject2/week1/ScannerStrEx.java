e8d60f02179a8396e9333b73d75f586885c2a6ef
shinjaewon99 <tlswodnjs99@naver.com>
2023-04-20 09:40:17 +0900

    feat : ScannerStr 구현 - 두개의 문자를 입력받아 더하기 구현

---
+++ src/main/java/com/example/javaproject2/week1/ScannerStrEx.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.week1;
+
+import java.util.Scanner;
+
+public class ScannerStrEx {
+
+    void inputStringAndPlus() {
+        Scanner in = new Scanner(System.in);
+        System.out.println(in.next() + in.next());
+    }
+}
---
+++ src/main/java/com/example/javaproject2/week1/ScannerStrExTest.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.week1;
+
+public class ScannerStrExTest {
+
+    public static void main(String[] args) {
+        ScannerStrEx strEx = new ScannerStrEx();
+        strEx.inputStringAndPlus();
+    }
+}
