4cec74cf87347a60e4c5b615ecd4f2bc3d9eb952
shinjaewon99 <tlswodnjs99@naver.com>
2023-04-27 14:33:51 +0900

    feat : CodeUp 1047 ~ 1048번 문제 - CodeUp 1047 ~ 1048번 문제 해결

---
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1047.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1047 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int inputNumber = in.nextInt();
+
+        System.out.println(inputNumber << 1);
+    }
+}
