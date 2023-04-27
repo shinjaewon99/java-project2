3cfeec737ee0fd3ce4ebcc8c9a9d4da0b1f0e2c1
shinjaewon99 <tlswodnjs99@naver.com>
2023-04-27 13:30:06 +0900

    feat : CodeUp 1053 ~ 1055번 문제 - CodeUp 1053 ~ 1055번 문제 해결

---
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1053.java
@@ -0,0 +1,17 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1053 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int inputNumber = in.nextInt();
+
+        if(inputNumber == 0){
+            System.out.println(1);
+        }else{
+            System.out.println(0);
+        }
+    }
+}
---
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1055.java
@@ -0,0 +1,15 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1055 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+
+        boolean check = in.nextInt() == 1 || in.nextInt() == 1;
+        System.out.println(check ? 1 : 0);
+
+
+    }
+}
