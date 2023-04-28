85ae6ff9de2fe892964ff84ccbcdc31c7756bdf0
shinjaewon99 <tlswodnjs99@naver.com>
2023-04-28 09:18:04 +0900

    feat : If, else 예제 구현 - if 조건문 구현

---
+++ src/main/java/com/example/javaproject2/week2/day5/ElseExam2.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.week2.day5;
+
+public class ElseExam2 {
+    public static void main(String[] args) {
+        int account = 1400;
+
+        if(account >= 1500){
+            System.out.println("1500을 차감합니다.");
+        }
+        System.out.println("잔액이 부족합니다.");
+    }
+}
---
+++ src/main/java/com/example/javaproject2/week2/day5/IfEx.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.week2.day5;
+
+public class IfEx {
+    public static void main(String[] args) {
+        if(true){
+            System.out.println("항상 실행됩니다.");
+        }
+    }
+}
