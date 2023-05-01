b58f252cb106d24e2531dff5b1a1b4a0d62e8ca6
shinjaewon99 <tlswodnjs99@naver.com>
2023-05-01 10:51:07 +0900

    feat : Loop for문 구현 - Loop for문 구현

---
+++ src/main/java/com/example/javaproject2/week3/day1/LoopEx1.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.week3.day1;
+
+public class LoopEx1 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 9; i++) {
+            System.out.println(i);
+        }
+    }
+}
