aa133f3f510c90b7f8e5db2f653b4943c44e638c
shinjaewon99 <tlswodnjs99@naver.com>
2023-05-01 09:07:56 +0900

    feat : CodeUp 1103, 1106번 문제 - CodeUp 1103, 1106번 문제 구현

---
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1106.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.codeup;
+
+public class CodeUp1106 {
+    public static void main(String[] args) {
+
+        int minValue = Integer.MIN_VALUE;
+        int maxValue = Integer.MAX_VALUE;
+        System.out.println(minValue + " " + maxValue);
+    }
+}
