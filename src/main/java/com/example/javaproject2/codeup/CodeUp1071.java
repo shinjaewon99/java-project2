7ce89a9d83d3a2adec540bd733386d7474a8b3e7
shinjaewon99 <tlswodnjs99@naver.com>
2023-05-01 10:49:44 +0900

    feat : CodeUp1071 ~ 1072번 문제 - CodeUp1071 ~ 1072번 문제 구현

---
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1071.java
@@ -0,0 +1,18 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1071 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        while(true){
+            int number = in.nextInt();
+
+            if(number == 0){
+                break;
+            }
+            System.out.println(number);
+        }
+    }
+}
---
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1072.java
@@ -0,0 +1,17 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1072 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int size = in.nextInt();
+
+        for(int i = 0 ; i < size; i++){
+            int number = in.nextInt();
+            System.out.println(number);
+        }
+    }
+}
