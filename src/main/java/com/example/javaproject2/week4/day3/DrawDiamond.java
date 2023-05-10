ffc083f56a2649ce1884702bcb291c657da71803
shinjaewon99 <tlswodnjs99@naver.com>
2023-05-10 09:53:29 +0900

    feat : 다이아몬드 찍기 문제 - 다이아몬드 찍기 문제 구현

---
+++ src/main/java/com/example/javaproject2/week4/day3/DrawDiamond.java
@@ -0,0 +1,37 @@
+package com.example.javaproject2.week4.day3;
+
+import java.util.Scanner;
+
+public class DrawDiamond {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int number = in.nextInt();
+        DrawDiamond drawDiamond = new DrawDiamond();
+        drawDiamond.upStair(number);
+        drawDiamond.downStair(number);
+    }
+
+    public void upStair(int number) {
+        for (int i = 1; i <= number; i += 2) {
+            for (int j = 0; j < (number - i) / 2; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public void downStair(int number) {
+        for (int i = number - 2; i >= 1; i -= 2) {
+            for (int j = 0; j < (number - i) / 2; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
