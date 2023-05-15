3ec076333d3a5b102a2983c81d3b4f8d6dba37f2
shinjaewon99 <tlswodnjs99@naver.com>
2023-05-15 09:51:34 +0900

    refactor : BubbleSort OOP 문제 구현 - BubbleSort OOP 문제 구현 구현

--- src/main/java/com/example/javaproject2/algorithm/BubbleSort.java
+++ src/main/java/com/example/javaproject2/algorithm/BubbleSort.java
@@ -3,19 +3,29 @@ package com.example.javaproject2.algorithm;
 import java.util.Arrays;
 
 public class BubbleSort {
+    private static int[] arr = {50, 40, 20, 30, 10};
 
-    public static void main(String[] args) {
-        int[] arr = {50, 40, 20, 30, 10};
 
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] > arr[j]) {
-                    int temp = arr[j];
-                    arr[j] = arr[i];
-                    arr[i] = temp;
-                }
+    public int[] sort(int[] arr, int loop){
+        for (int j = loop + 1; j < arr.length; j++) {
+            if (arr[loop] > arr[j]) {
+                int temp = arr[j];
+                arr[j] = arr[loop];
+                arr[loop] = temp;
             }
         }
+        return arr;
+    }
+
+    public void loop(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            sort(arr, i);
+        }
+    }
+
+    public static void main(String[] args) {
+        BubbleSort sort = new BubbleSort();
+        sort.loop(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
