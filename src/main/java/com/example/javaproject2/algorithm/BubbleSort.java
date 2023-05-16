3cf6577dc14826a594f9fb490045ff07040d7d29
shinjaewon99 <tlswodnjs99@naver.com>
2023-05-16 10:05:55 +0900

    refactor : 버블정렬 주석 추가 - 버블정렬 주석 추가

--- src/main/java/com/example/javaproject2/algorithm/BubbleSort.java
+++ src/main/java/com/example/javaproject2/algorithm/BubbleSort.java
@@ -3,29 +3,37 @@ package com.example.javaproject2.algorithm;
 import java.util.Arrays;
 
 public class BubbleSort {
-    private static int[] arr = {50, 40, 20, 30, 10};
+    // private static final 상수 : 대문자로 명명
+    private static final int[] NUMBERS = {35, 10, 50, 40, 20};
 
-
-    public int[] sort(int[] arr, int loop){
-        for (int j = loop + 1; j < arr.length; j++) {
-            if (arr[loop] > arr[j]) {
-                int temp = arr[j];
-                arr[j] = arr[loop];
-                arr[loop] = temp;
-            }
+    // 정렬 메소드
+    public int[] sortNumbers(int[] number, int loop) {
+        for (int j = loop + 1; j < number.length; j++) {
+            compareNumbers(loop, j);
         }
-        return arr;
+        return number;
     }
 
-    public void loop(int[] arr){
-        for (int i = 0; i < arr.length; i++) {
-            sort(arr, i);
+    // 비교 메소드
+    public void compareNumbers(int loop1, int loop2) {
+        if (NUMBERS[loop1] > NUMBERS[loop2]) {
+            int temp = NUMBERS[loop1];
+            NUMBERS[loop1] = NUMBERS[loop2];
+            NUMBERS[loop2] = temp;
         }
     }
 
+    // 반복 메소드
+    public void loop(int[] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            sortNumbers(numbers, i);
+        }
+        System.out.println(Arrays.toString(numbers));
+    }
+
+    // 호출과 생성만
     public static void main(String[] args) {
         BubbleSort sort = new BubbleSort();
-        sort.loop(arr);
-        System.out.println(Arrays.toString(arr));
+        sort.loop(NUMBERS);
     }
 }
