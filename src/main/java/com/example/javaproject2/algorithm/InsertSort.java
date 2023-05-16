4645c098681886e63414bdd780c3cf348265baaf
shinjaewon99 <tlswodnjs99@naver.com>
2023-05-16 10:18:48 +0900

    refactor : 삽입정렬 내림차순 기능 추가 - 삽입정렬 내림차순 기능 추가

--- src/main/java/com/example/javaproject2/algorithm/InsertSort.java
+++ src/main/java/com/example/javaproject2/algorithm/InsertSort.java
@@ -11,6 +11,27 @@ public class InsertSort {
         }
     }
 
+    public void sort(int[] numbers, boolean isDesc) {
+        if(isDesc){
+            reverseSort(numbers);
+        }
+    }
+
+    private void reverseSort(int[] numbers) {
+        sort(numbers); // 오름차순으로 정렬을 먼저 해준다.
+        int start = 0;
+        int end = numbers.length - 1;
+
+        while (start < end) {
+            int temp = numbers[start];
+            numbers[start] = numbers[end];
+            numbers[end] = temp;
+            start++;
+            end--;
+        }
+
+    }
+
     public void shiftNumber(int[] numbers, int pivot, int j) {
         while (j >= 0 && numbers[j] > pivot) {
             numbers[j + 1] = numbers[j];
@@ -25,7 +46,10 @@ public class InsertSort {
         int[] numbers = {50, 40, 10, 20, 60};
 
         sort.sort(numbers);
-
         System.out.println(Arrays.toString(numbers));
+
+        sort.sort(numbers,true);
+        System.out.println(Arrays.toString(numbers));
+
     }
 }
